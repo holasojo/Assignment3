39cbe186ebf8630e65afc2d2356ace94e1f6b595
holasojo <joso930115@gmail.com>
2016-04-07 21:25:24 -0400

    File /Users/SOJO/Documents/eclipse_workspace/Assignment3/src/StatFileWriter.java changed at Thu Apr 07 21:25:24 EDT 2016

--- src/StatFileWriter.java
+++ src/StatFileWriter.java
@@ -6,13 +6,12 @@ public class StatFileWriter {
     FileWriter writer;
     BufferedWriter bw;
     BufferPool pool;
-    
 
     public StatFileWriter(String statFile, BufferPool b) throws IOException {
         writer = new FileWriter(statFile, true);
         bw = new BufferedWriter(writer);
         pool = b;
-        
+
     }
 
     public void write(String filename, double time) throws IOException {
@@ -20,11 +19,11 @@ public class StatFileWriter {
         bw.write("Cache Hits:   " + pool.getCacheHits() + '\n');
         bw.write("Cache Misses: " + pool.getCacheMisses() + '\n');
         bw.write("Disk Reads:   " + pool.getDiskReads() + '\n');
-        bw.write("Disk Writes:  " + pool.getDiskWrites()+ '\n');
+        bw.write("Disk Writes:  " + pool.getDiskWrites() + '\n');
         bw.write("Time (ms):    " + time + '\n');
         bw.write('\n');
         bw.close();
-        
+
     }
 
 }
