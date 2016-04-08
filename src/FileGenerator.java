70552dbce0b50c211a3ebac38ee6188159f7d878
holasojo <joso930115@gmail.com>
2016-04-07 21:25:12 -0400

    File /Users/SOJO/Documents/eclipse_workspace/Assignment3/src/FileGenerator.java changed at Thu Apr 07 21:25:12 EDT 2016

--- src/FileGenerator.java
+++ src/FileGenerator.java
@@ -17,14 +17,12 @@ import java.util.*;
  * @author modified by Sebastian Welsh (welshs16)
  * @version Sep 28, 2014
  */
-public class FileGenerator
-{
-    private static final int NUM_RECS = 2048;                // Because they are
-                                                             // short
+public class FileGenerator {
+    private static final int NUM_RECS = 2048; // Because they are
+                                              // short
 
     /** Initialize the random variable */
-    static private Random    value    = new Random(); // Hold the Random class
-
+    static private Random value = new Random(); // Hold the Random class
 
     /**
      * This function generates a random number.
@@ -33,11 +31,9 @@ public class FileGenerator
      *            the ceiling
      * @return a random number
      */
-    public int random(int n)
-    {
+    public int random(int n) {
         return Math.abs(value.nextInt()) % n;
     }
-
 
     /**
      * This method generates a file.
@@ -47,39 +43,28 @@ public class FileGenerator
      * @throws IOException
      *             an exception
      */
-    public void generateFile(String[] args)
-        throws IOException
-    {
+    public void generateFile(String[] args) throws IOException {
         short val;
         int filesize = Integer.parseInt(args[2]); // Size of file in blocks
-        DataOutputStream file =
-            new DataOutputStream(new BufferedOutputStream(new FileOutputStream(
-                args[1])));
+        DataOutputStream file = new DataOutputStream(
+                new BufferedOutputStream(new FileOutputStream(args[1])));
 
-        if (args[0].charAt(1) == 'b')
-        { // Write out random numbers
-            for (int i = 0; i < filesize; i++)
-            {
-                for (int j = 0; j < NUM_RECS; j++)
-                {
-                    val = (short)(random(29999) + 1);
+        if (args[0].charAt(1) == 'b') { // Write out random numbers
+            for (int i = 0; i < filesize; i++) {
+                for (int j = 0; j < NUM_RECS; j++) {
+                    val = (short) (random(29999) + 1);
                     file.writeShort(val);
                 }
             }
         }
-        else if (args[0].charAt(1) == 'a')
-        { // Write out ASCII-readable values
-            for (int i = 0; i < filesize; i++)
-            {
-                for (int j = 0; j < NUM_RECS; j++)
-                {
-                    if ((j % 2) == 1)
-                    {
-                        val = (short)(8224);
+        else if (args[0].charAt(1) == 'a') { // Write out ASCII-readable values
+            for (int i = 0; i < filesize; i++) {
+                for (int j = 0; j < NUM_RECS; j++) {
+                    if ((j % 2) == 1) {
+                        val = (short) (8224);
                     }
-                    else
-                    {
-                        val = (short)(random(26) + 0x2041);
+                    else {
+                        val = (short) (random(26) + 0x2041);
                     }
                     file.writeShort(val);
                 }
