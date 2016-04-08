8d671c704f5b0ad8226d0e4bc2af5aa026bb359b
holasojo <joso930115@gmail.com>
2016-04-07 21:24:55 -0400

    File /Users/SOJO/Documents/eclipse_workspace/Assignment3/src/BufferPoolTest.java changed at Thu Apr 07 21:24:55 EDT 2016

--- src/BufferPoolTest.java
+++ src/BufferPoolTest.java
@@ -9,15 +9,12 @@ import junit.framework.TestCase;
 
  * 
  */
-public class BufferPoolTest extends TestCase
-{
-    
-    public void testBufferAlterations() throws IOException
-    {
+public class BufferPoolTest extends TestCase {
+
+    public void testBufferAlterations() throws IOException {
         RandomAccessFile file = new RandomAccessFile("test", "rw");
         byte[] byte1 = new byte[20];
-        for ( int j = 0; j < 20; j++ )
-        {
+        for (int j = 0; j < 20; j++) {
             byte1[j] = (byte) j;
         }
 
@@ -25,14 +22,14 @@ public class BufferPoolTest extends TestCase
 
         BufferPool pool = new BufferPool(file, 10);
 
-        pool.getbytes( byte1, 20, 3 , file);
-        pool.insert( byte1, 20, 3 , file);
+        pool.getbytes(byte1, 20, 3, file);
+        pool.insert(byte1, 20, 3, file);
 
         System.out.println(byte1[8]);
         System.out.println(byte2[8]);
-        
-        assertEquals( byte1[9], byte2[9] );
+
+        assertEquals(byte1[9], byte2[9]);
     }
 
-//   
+    //
 }
